ee3bf7cd023201859ed7e0edb7a7a779a3b512e3
Jefferson Leard <jtleard@gmail.com>
2013-11-06 14:36:42 -0800

    Update seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ActiveRecord::Base.transaction do
+	ActivityBase.create({ name: "Water Running", description: "To splash is to err.", set_type: "DistanceSet" })
+	ActivityBase.create({ name: "Stealth", description: "Ninja vanish!",
+												set_type: "DurationSet" })
+	ActivityBase.create({ name: "Taijutsu", description: "The lotus of Konoha blooms twice.", set_type: "GeneralSet" })
+	ActivityBase.create({ name: "Boulder Toss", description: "Sorry I'm late, I'm afraid I got lost on the path of life.", set_type: "WeightSet" })
+	ActivityBase.create({ name: "Genjutsu", description: "How many times do I have to tell you? The first move is always a feint.", set_type: "RepetitionSet" })
 
-ActivityBase.create({ name: "Water Running", description: "To splash is to err.", set_type: "DistanceSet" })
-ActivityBase.create({ name: "Stealth", description: "Ninja vanish!",
-											set_type: "DurationSet" })
-ActivityBase.create({ name: "Taijutsu", description: "The lotus of Konoha blooms twice.", set_type: "GeneralSet" })
-ActivityBase.create({ name: "Boulder Toss", description: "Sorry I'm late, I'm afraid I got lost on the path of life.", set_type: "WeightSet" })
-ActivityBase.create({ name: "Genjutsu", description: "How many times do I have to tell you? The first move is always a feint.", set_type: "RepetitionSet" })
+	guest = User.create({ email: "guest@mail.com", password: "password" })
 
-guest = User.create({ email: "guest@mail.com", password: "password" })
+	workout = guest.workouts.create({logged_date: Date.today})
 
-guest.workouts.create({logged_date: Date.today})
+	act1 = workout.activities.create({ activity_base_id:1 })
+	act2 = workout.activities.create({ activity_base_id:2 })
+	act3 = workout.activities.create({ activity_base_id:5 })
+
+	act1.activity_sets << act1.activity_base.set_type.constantize.create({
+		distance:50, duration:300, set_number:1 })
+	act1.activity_sets << act1.activity_base.set_type.constantize.create({
+		distance:100, duration:450, set_number:2 })
+	act1.activity_sets << act1.activity_base.set_type.constantize.create({
+		distance:50, duration:300, set_number:3 })
+
+	act2.activity_sets << act2.activity_base.set_type.constantize.create({
+		duration:300, set_number:1 })
+	act2.activity_sets << act2.activity_base.set_type.constantize.create({
+		duration:3600, set_number:2 })
+
+	act3.activity_sets << act3.activity_base.set_type.constantize.create({
+		repetitions:15, set_number:1 })
+	act3.activity_sets << act3.activity_base.set_type.constantize.create({
+		repetitions:15, set_number:2 })
+end
